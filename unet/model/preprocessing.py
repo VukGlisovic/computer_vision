92490776982de1fcc6681537c3e0378381a1e2b2
¨Vuk <GlisovicVuk@gmail.com>
2019-10-14 21:58:40 +0200

    added logging

--- unet/model/preprocessing.py
+++ unet/model/preprocessing.py
@@ -1,5 +1,6 @@
 from unet.model.constants import *
 from glob import glob
+import numpy as np
 import cv2
 import tensorflow as tf
 
@@ -9,14 +10,15 @@ def load_data(image_path_glob='../data/images/*', mask_path_glob='../data/masks/
     train_image_files = glob(image_path_glob)
     train_mask_files = glob(mask_path_glob)
     # load the image data
-    Xtrain = [cv2.imread(p) for p in train_image_files]
-    ytrain = [cv2.imread(p) for p in train_mask_files]
+    Xtrain = np.array([cv2.imread(p)[:, :, :1] for p in train_image_files])  # only one of the three channels is needed
+    ytrain = np.array([cv2.imread(p)[:, :, :1] for p in train_mask_files])
     return Xtrain, ytrain
 
 
-def preprocess_image(image):
-    image = image / 255.
-    return image
+def preprocess_image(image, mask):
+    image = tf.dtypes.cast(image, dtype=tf.dtypes.float32) / 255.
+    mask = tf.dtypes.cast(mask, dtype=tf.dtypes.float32) / 255.
+    return image, mask
 
 
 def input_fn(Xtrain, ytrain, epochs=None, batch_size=32, shuffle_buffer=None):
--- unet/model/train.py
+++ unet/model/train.py
@@ -4,29 +4,47 @@ from unet.model.architecture import *
 from unet.model.metrics import iou
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+import sys
+import logging
+
+logformat = '%(asctime)s | %(levelname)s | [%(filename)s:%(lineno)s - %(funcName)s] %(message)s'
+logging.basicConfig(format=logformat, level=logging.INFO, stream=sys.stdout)
 
 
 def get_model():
     input_image = keras.layers.Input(IMAGE_SHAPE, name='image')
     unet_model = create_unet_model(input_image, batchnorm=False)
 
-    model = keras.Model(input_image, unet_model)
-    optimizer = keras.optimizers.adam(lr=0.01)
-    model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=[iou])
-    return model
+    optimizer = keras.optimizers.Adam(lr=0.01)
+    unet_model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=[iou])
+    return unet_model
 
 
 def train_and_validate(model):
-    Xdata, ydata = load_data()
-    Xtrain, Xvalid, ytrain, yvalid = train_test_split(Xdata, ydata)
-    train_dataset = input_fn(Xtrain, ytrain, epochs=2, batch_size=32, shuffle_buffer=300)
-    valid_dataset = input_fn(Xvalid, yvalid, epochs=None, batch_size=128, shuffle_buffer=None)
+    logging.info("Loading the data.")
+    Xdata, ydata = load_data()  # expecting 4000 samples
+    logging.info("Splitting the data into train and validation set.")
+    train_size = 3200
+    Xtrain, Xvalid, ytrain, yvalid = train_test_split(Xdata, ydata, train_size=train_size)
+    logging.info("Creating train data input_fn.")
+    train_batch_size = 32
+    train_dataset = input_fn(Xtrain, ytrain, epochs=2, batch_size=train_batch_size, shuffle_buffer=300)
+    logging.info("Creating validation data input_fn.")
+    valid_batch_size = 200
+    valid_dataset = input_fn(Xvalid, yvalid, epochs=None, batch_size=valid_batch_size, shuffle_buffer=None)
 
+    logging.info("Creating keras callbacks.")
     model_checkpoint = keras.callbacks.ModelCheckpoint('unet_saved_model', monitor='iou', mode='max', save_best_only=True, verbose=1)
+
+    logging.info("Start training...")
+    steps_per_epoch = train_size // train_batch_size
     model.fit(train_dataset,
+              steps_per_epoch=steps_per_epoch,
               validation_data=valid_dataset,
+              validation_steps=4,  # 4 steps of 200 samples covers the entire validation set
               callbacks=[model_checkpoint],
               verbose=2)
+    logging.info("Finished training!")
 
 
 if __name__ == '__main__':
