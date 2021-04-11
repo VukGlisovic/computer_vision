c7782c7dabc1cac062419be7b1171075d7b4078a
v.glisovic <v.glisovic@fourthline.com>
2021-04-11 21:29:22 +0200

    updated notebook for bbox creating data

---
+++ efficientdet/constants.py
@@ -0,0 +1,6 @@
+import os
+
+
+# project information
+PROJECT_NAME = 'efficientdet'
+PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
