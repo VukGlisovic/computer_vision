c3758496069e72904fa7a26b210750554714b628
v.glisovic <v.glisovic@fourthline.com>
2021-05-07 13:22:41 +0200

    initial version

---
+++ efficientdet/data_pipeline/utils.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+
+def xyxy_to_xywh(boxes):
+    """Transforms [xmin, ymin, xmax, ymax] to [center x, center y, width, height].
+
+    Args:
+        boxes (tf.Tensor):
+
+    Returns:
+        tf.Tensor
+    """
+    boxes = tf.cast(boxes, dtype=tf.float32)
+    boxes_transformed = tf.concat(
+        [(boxes[..., :2] + boxes[..., 2:]) / 2.0, boxes[..., 2:] - boxes[..., :2]],
+        axis=-1
+    )
+    return boxes_transformed
+
+
+def xywh_to_xyxy(boxes):
+    """Transforms [center x, center y, width, height] to [xmin, ymin, xmax, ymax].
+
+    Args:
+      boxes (tf.Tensor):
+
+    Returns:
+        tf.Tensor
+    """
+    boxes_transformed = tf.concat(
+        [boxes[..., :2] - boxes[..., 2:] / 2.0, boxes[..., :2] + boxes[..., 2:] / 2.0],
+        axis=-1
+    )
+    return boxes_transformed
---
+++ efficientdet/model/efficientdet_heads.py
@@ -0,0 +1,137 @@
+import math
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models
+from tensorflow.keras import initializers
+
+
+MOMENTUM = 0.997
+EPSILON = 1e-4
+
+
+class PriorProbability(keras.initializers.Initializer):
+    """ Apply a prior probability to the weights.
+    """
+
+    def __init__(self, probability=0.01):
+        self.probability = probability
+
+    def get_config(self):
+        return {
+            'probability': self.probability
+        }
+
+    def __call__(self, shape, dtype=None):
+        # set bias to -log((1 - p)/p) for foreground
+        result = np.ones(shape, dtype=np.float32) * -math.log((1 - self.probability) / self.probability)
+
+        return result
+
+
+class BoxNet(models.Model):
+    def __init__(self, width, depth, num_anchors=9, separable_conv=True, freeze_bn=False, detect_quadrangle=False, **kwargs):
+        super(BoxNet, self).__init__(**kwargs)
+        self.width = width
+        self.depth = depth
+        self.num_anchors = num_anchors
+        self.separable_conv = separable_conv
+        self.detect_quadrangle = detect_quadrangle
+        num_values = 9 if detect_quadrangle else 4
+        options = {
+            'kernel_size': 3,
+            'strides': 1,
+            'padding': 'same',
+            'bias_initializer': 'zeros',
+        }
+        if separable_conv:
+            kernel_initializer = {
+                'depthwise_initializer': initializers.VarianceScaling(),
+                'pointwise_initializer': initializers.VarianceScaling(),
+            }
+            options.update(kernel_initializer)
+            self.convs = [layers.SeparableConv2D(filters=width, name=f'{self.name}/box-{i}', **options) for i in range(depth)]
+            self.head = layers.SeparableConv2D(filters=num_anchors * num_values, name=f'{self.name}/box-predict', **options)
+        else:
+            kernel_initializer = {
+                'kernel_initializer': initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None)
+            }
+            options.update(kernel_initializer)
+            self.convs = [layers.Conv2D(filters=width, name=f'{self.name}/box-{i}', **options) for i in range(depth)]
+            self.head = layers.Conv2D(filters=num_anchors * num_values, name=f'{self.name}/box-predict', **options)
+        self.bns = [[layers.BatchNormalization(momentum=MOMENTUM, epsilon=EPSILON, name=f'{self.name}/box-{i}-bn-{j}') for j in range(3, 8)]
+                    for i in range(depth)]
+        # self.bns = [[BatchNormalization(freeze=freeze_bn, name=f'{self.name}/box-{i}-bn-{j}') for j in range(3, 8)]
+        #             for i in range(depth)]
+        self.relu = layers.Lambda(lambda x: tf.nn.swish(x))
+        self.reshape = layers.Reshape((-1, num_values))
+        self.level = 0
+
+    def call(self, inputs, **kwargs):
+        feature, level = inputs
+        for i in range(self.depth):
+            feature = self.convs[i](feature)
+            # feature = self.bns[i][self.level](feature)
+            feature = self.bns[i][level](feature)
+            feature = self.relu(feature)
+        outputs = self.head(feature)
+        outputs = self.reshape(outputs)
+        # self.level += 1
+        return outputs
+
+
+class ClassNet(models.Model):
+    def __init__(self, width, depth, num_classes=20, num_anchors=9, separable_conv=True, freeze_bn=False, **kwargs):
+        super(ClassNet, self).__init__(**kwargs)
+        self.width = width
+        self.depth = depth
+        self.num_classes = num_classes
+        self.num_anchors = num_anchors
+        self.separable_conv = separable_conv
+        options = {
+            'kernel_size': 3,
+            'strides': 1,
+            'padding': 'same',
+        }
+        if self.separable_conv:
+            kernel_initializer = {
+                'depthwise_initializer': initializers.VarianceScaling(),
+                'pointwise_initializer': initializers.VarianceScaling(),
+            }
+            options.update(kernel_initializer)
+            self.convs = [layers.SeparableConv2D(filters=width, bias_initializer='zeros', name=f'{self.name}/class-{i}', **options)
+                          for i in range(depth)]
+            self.head = layers.SeparableConv2D(filters=num_classes * num_anchors,
+                                               bias_initializer=PriorProbability(probability=0.01),
+                                               name=f'{self.name}/class-predict', **options)
+        else:
+            kernel_initializer = {
+                'kernel_initializer': initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None)
+            }
+            options.update(kernel_initializer)
+            self.convs = [layers.Conv2D(filters=width, bias_initializer='zeros', name=f'{self.name}/class-{i}', **options)
+                          for i in range(depth)]
+            self.head = layers.Conv2D(filters=num_classes * num_anchors,
+                                      bias_initializer=PriorProbability(probability=0.01),
+                                      name='class-predict', **options)
+        self.bns = [[layers.BatchNormalization(momentum=MOMENTUM, epsilon=EPSILON, name=f'{self.name}/class-{i}-bn-{j}') for j in range(3, 8)]
+                    for i in range(depth)]
+        # self.bns = [[BatchNormalization(freeze=freeze_bn, name=f'{self.name}/class-{i}-bn-{j}') for j in range(3, 8)]
+        #             for i in range(depth)]
+        self.relu = layers.Lambda(lambda x: tf.nn.swish(x))
+        self.reshape = layers.Reshape((-1, num_classes))
+        self.activation = layers.Activation('sigmoid')
+        self.level = 0
+
+    def call(self, inputs, **kwargs):
+        feature, level = inputs
+        for i in range(self.depth):
+            feature = self.convs[i](feature)
+            # feature = self.bns[i][self.level](feature)
+            feature = self.bns[i][level](feature)
+            feature = self.relu(feature)
+        outputs = self.head(feature)
+        outputs = self.reshape(outputs)
+        outputs = self.activation(outputs)
+        # self.level += 1
+        return outputs
