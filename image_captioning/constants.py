0944b14e53346e992f0de035782dc2d2c765f1b3
v.glisovic <v.glisovic@fourthline.com>
2022-06-05 10:37:21 +0200

    Created DATA_DIR constant

--- image_captioning/constants.py
+++ image_captioning/constants.py
@@ -5,3 +5,5 @@ import tensorflow as tf
 # project information
 PROJECT_NAME = 'image_captioning'
 PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
+
+DATA_DIR = os.path.join(PROJECT_PATH, 'data')
