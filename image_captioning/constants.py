98277e2d8ed243325f23a904833cafa4ee9c9972
v.glisovic <v.glisovic@fourthline.com>
2022-06-11 13:16:12 +0200

    added constants

--- image_captioning/constants.py
+++ image_captioning/constants.py
@@ -7,3 +7,5 @@ PROJECT_NAME = 'image_captioning'
 PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
 
 DATA_DIR = os.path.join(PROJECT_PATH, 'data')
+ANNOTATION_FILE = os.path.join(DATA_DIR, 'annotations/captions_train2014.json')
+IMAGES_DIR = os.path.join(DATA_DIR, 'train2014')
