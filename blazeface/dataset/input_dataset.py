e5ac16c81f57384e28cad230f585cd75b47e8ca5
v.glisovic <v.glisovic@fourthline.com>
2022-09-22 21:30:50 +0200

    updated tfds save dir

--- blazeface/dataset/input_dataset.py
+++ blazeface/dataset/input_dataset.py
@@ -1,7 +1,10 @@
+import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+from blazeface.constants import DATA_DIR
+
 
 def load_the300w_lp(split="train[:80%]"):
-    dataset, info = tfds.load("the300w_lp", split=split, with_info=True)
+    dataset, info = tfds.load("the300w_lp", split=split, data_dir=os.path.join(DATA_DIR, "tensorflow_datasets"), with_info=True)
     return dataset, info
