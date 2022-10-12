ab4b418df3b4161747a7db2398def9ecf1a3b4a9
v.glisovic <v.glisovic@fourthline.com>
2022-10-12 10:52:26 +0200

    added constants

--- blazeface/constants.py
+++ blazeface/constants.py
@@ -9,4 +9,8 @@ DATA_DIR = os.path.join(PROJECT_PATH, 'data')
 
 IMG_SIZE = 128
 BBOX_PADDING = 1e-3
-
+BOX_VARIANCE = [0.1, 0.1, 0.2, 0.2]
+N_LANDMARKS = 6
+FMAPS_WITH_ANCHORS = [16, 8]  # which feature map sizes have anchors
+N_ANCHORS_PER_LOC = [2, 6]  # number of anchor predictions per location in the 16x16 and 8x8 feature maps respectively
+assert len(FMAPS_WITH_ANCHORS) == len(N_ANCHORS_PER_LOC)
