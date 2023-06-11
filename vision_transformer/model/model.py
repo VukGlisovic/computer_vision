751bc0b26b18b2978f25351e8a4fadcc337b9e14
v.glisovic <v.glisovic@fourthline.com>
2023-06-11 20:22:32 +0200

    updated model creation

--- vision_transformer/model/model.py
+++ vision_transformer/model/model.py
@@ -15,30 +15,24 @@ mlp_dim = 128
 
 ######################################
 
-rescale_layer = tf.keras.Sequential([tf.keras.layers.experimental.preprocessing.Rescaling(1./255)])
-
 
 def build_ViT(input_shape):
     inputs = tf.keras.layers.Input(shape=input_shape)
-    # rescaling (normalizing pixel val between 0 and 1)
-    rescale = rescale_layer(inputs)
-    # generate patches with conv layer
-    patch_encodings = custom_layers.build_image_patches_encoder(patch_size, hidden_size, rescale)
+    input_rescale = tf.keras.layers.Rescaling(scale=1/255)(inputs)  # rescale to the [0, 1] range
 
-    ######################################
-    # ready for the transformer blocks
-    ######################################
-    encoder_out = custom_layers.vit_encoder(transformer_layers, mlp_dim, num_heads, patch_encodings)
+    # create the image patches encoder
+    initial_patch_encodings = custom_layers.build_image_patches_encoder(patch_size, hidden_size, input_rescale)
 
-    #####################################
-    #  final part (mlp to classification)
-    #####################################
-    #encoder_out_rank = int(tf.experimental.numpy.ndim(encoder_out))
-    im_representation = tf.reduce_mean(encoder_out, axis=1)  # (1,) or (1,2)
-    # similar to the GAP, this is from original Google GitHub
+    # build the ViT encoder
+    encoder_out = custom_layers.vit_encoder(transformer_layers, mlp_dim, num_heads, initial_patch_encodings)
 
-    logits = tf.keras.layers.Dense(units=len(CIFAR10_CLASSES), name='head', kernel_initializer=tf.keras.initializers.zeros)(im_representation)
-    # !!! important !!! activation is linear
+    # add the classification layer
+    # im_representation = tf.reduce_mean(encoder_out, axis=1)  # (1,) or (1,2)
+    backbone_extracted_features = tf.keras.layers.GlobalAveragePooling1D()(encoder_out)
 
-    final_model = tf.keras.Model(inputs = inputs, outputs = logits)
-    return final_model
+    # note the linear activation
+    logits = tf.keras.layers.Dense(units=len(CIFAR10_CLASSES), activation='linear', name='head',
+                                   kernel_initializer=tf.keras.initializers.zeros)(backbone_extracted_features)
+
+    vit_model = tf.keras.Model(inputs=inputs, outputs=logits)
+    return vit_model
