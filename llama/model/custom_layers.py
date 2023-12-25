39dd38cc0de0fb5e56e76b702f4998d1bfe0bfb5
v.glisovic <v.glisovic@fourthline.com>
2023-12-25 10:39:16 +0100

    created custom layers module

---
+++ llama/model/custom_layers.py
@@ -0,0 +1,146 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class RMSNorm(nn.Module):
+
+    def __init__(self, layer_shape, eps=1e-8):
+        super().__init__()
+        self.layer_shape = layer_shape
+        self.eps = eps
+
+        # Registering a learnable parameter 'scale' as a parameter of the module
+        self.register_parameter("scale", nn.Parameter(torch.ones(layer_shape)))
+
+    def forward(self, x):
+        """
+        Assumes shape is (batch, seq_len, d_model)
+        """
+        # Calculating the Frobenius norm, RMS = 1/sqrt(N) * Frobenius norm
+        ff_rms = torch.linalg.norm(x, dim=(1,2)) * x[0].numel() ** -.5
+
+        # Normalizing the input tensor 'x' with respect to RMS
+        raw = x / (ff_rms.unsqueeze(-1).unsqueeze(-1) + self.eps)
+
+        # Scaling the normalized tensor using the learnable parameter 'scale'
+        return self.scale[:x.shape[1], :].unsqueeze(0) * raw
+
+
+class RoPEAttentionHead(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        # Linear transformation for query
+        self.w_q = nn.Linear(config['d_model'], config['d_model'], bias=False)
+        # Linear transformation for key
+        self.w_k = nn.Linear(config['d_model'], config['d_model'], bias=False)
+        # Linear transformation for value
+        self.w_v = nn.Linear(config['d_model'], config['d_model'], bias=False)
+        # Obtain rotary matrix for positional embeddings
+        self.R = self.get_rotary_matrix(config['context_window'], config['d_model'])
+
+        # Using nn.Parameter for R to ensure model.to(device) moves this to GPU as well
+        self.R = nn.Parameter(self.R, requires_grad=False)
+        self.register_parameter("R", self.R)
+
+    @staticmethod
+    def get_rotary_matrix(context_window, embedding_dim):
+        # Initialize a tensor for the rotary matrix with zeros
+        R = torch.zeros((context_window, embedding_dim, embedding_dim), requires_grad=False)
+
+        # Loop through each position in the context window
+        for position in range(context_window):
+            # Loop through each dimension in the embedding
+            for i in range(embedding_dim // 2):
+                # Calculate the rotation angle (theta) based on the position and embedding dimension
+                theta = 10000. ** (-2. * (i - 1) / embedding_dim)
+                # Calculate the rotated matrix elements using sine and cosine functions
+                m_theta = position * theta
+                R[position, 2 * i, 2 * i] = np.cos(m_theta)
+                R[position, 2 * i, 2 * i + 1] = -np.sin(m_theta)
+                R[position, 2 * i + 1, 2 * i] = np.sin(m_theta)
+                R[position, 2 * i + 1, 2 * i + 1] = np.cos(m_theta)
+        return R
+
+    def forward(self, x, return_attn_weights=False):
+        # x: input tensor of shape (batch, sequence length, dimension)
+
+        b, m, d = x.shape  # batch size, sequence length, dimension
+
+        # Linear transformations for Q, K, and V
+        q = self.w_q(x)
+        k = self.w_k(x)
+        v = self.w_v(x)
+
+        # Rotate Q and K using the RoPE matrix
+        q_rotated = (torch.bmm(q.transpose(0, 1), self.R[:m])).transpose(0, 1)
+        k_rotated = (torch.bmm(k.transpose(0, 1), self.R[:m])).transpose(0, 1)
+
+        # Perform scaled dot-product attention
+        activations = F.scaled_dot_product_attention(
+            q_rotated, k_rotated, v, dropout_p=0.1, is_causal=True
+        )
+
+        if return_attn_weights:
+            # Create a causal attention mask
+            attn_mask = torch.tril(torch.ones((m, m)), diagonal=0)
+            # Calculate attention weights and add causal mask
+            attn_weights = torch.bmm(q_rotated, k_rotated.transpose(1, 2)) / np.sqrt(d) + attn_mask
+            attn_weights = F.softmax(attn_weights, dim=-1)
+            return activations, attn_weights
+
+        return activations
+
+
+class RoPEMaskedMultiheadAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        # Create a list of RoPEMaskedAttentionHead instances as attention heads
+        self.heads = nn.ModuleList([
+            RoPEAttentionHead(config) for _ in range(config['n_heads'])
+        ])
+        self.linear = nn.Linear(config['n_heads'] * config['d_model'],
+                                config['d_model'])  # Linear layer after concatenating heads
+        self.dropout = nn.Dropout(0.1)  # Dropout layer
+
+    def forward(self, x):
+        # x: input tensor of shape (batch, sequence length, dimension)
+
+        # Process each attention head and concatenate the results
+        heads = [h(x) for h in self.heads]
+        x = torch.cat(heads, dim=-1)
+
+        # Apply linear transformation to the concatenated output
+        x = self.linear(x)
+
+        # Apply dropout
+        x = self.dropout(x)
+        return x
+
+
+class SwiGLU(nn.Module):
+    """Paper: https://arxiv.org/pdf/2002.05202v1.pdf
+    """
+
+    def __init__(self, size):
+        super().__init__()
+        self.size = size  # Configuration information
+
+        self.linear_gate = nn.Linear(size, size)  # Linear transformation for the gating mechanism
+        self.linear = nn.Linear(size, size)  # Linear transformation for the main branch
+        self.beta = torch.randn(1, requires_grad=True)  # Random initialization of the beta parameter
+
+        # Using nn.Parameter for beta to ensure it's recognized as a learnable parameter
+        self.beta = nn.Parameter(torch.ones(1))
+        self.register_parameter("beta", self.beta)
+
+    def forward(self, x):
+        # Swish-Gated Linear Unit computation
+        swish_gate = self.linear_gate(x) * torch.sigmoid(self.beta * self.linear_gate(x))
+        out = swish_gate * self.linear(x)  # Element-wise multiplication of the gate and main branch
+        return out
