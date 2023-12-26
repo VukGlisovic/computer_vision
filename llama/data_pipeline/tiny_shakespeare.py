1777f20b5d8266d8cf7c08b33c3dc7dff8c048f8
v.glisovic <v.glisovic@fourthline.com>
2023-12-26 20:45:28 +0100

    created simple method for downloading tiny shakespeare

---
+++ llama/data_pipeline/tiny_shakespeare.py
@@ -0,0 +1,17 @@
+import os
+import urllib
+
+from llama.constants import DATA_DIR
+
+
+def download_tiny_shakespeare(file_name="tinyshakespeare.txt"):
+    """Downloads tiny shakespeare dataset and saves it to disk. Returns the
+    path to the saved file.
+    """
+    # the URL of the raw text file on GitHub
+    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+    # store in DATA_DIR
+    os.makedirs(DATA_DIR, exist_ok=True)
+    save_path = os.path.join(DATA_DIR, file_name)
+    urllib.request.urlretrieve(url, save_path)
+    return save_path
