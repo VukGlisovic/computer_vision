d9d57db7a849f89c03e549878f763fb4b55ba80c
v.glisovic <v.glisovic@fourthline.com>
2023-12-28 17:36:42 +0100

    updated constants

--- llama/constants.py
+++ llama/constants.py
@@ -5,4 +5,4 @@ PROJECT_NAME = 'llama'
 PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
 
 DATA_DIR = os.path.join(PROJECT_PATH, 'data')
-EXPERIMENT_DIR = os.path.join(DATA_DIR, 'experiment')
+EXPERIMENTS_DIR = os.path.join(PROJECT_PATH, 'experiments')
