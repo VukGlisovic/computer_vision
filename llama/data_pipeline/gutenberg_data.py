7d4975987ba2f38323bfcc8ebaa505b157bbbd35
v.glisovic <v.glisovic@fourthline.com>
2024-01-04 22:14:58 +0100

    created module for gutenberg data

---
+++ llama/data_pipeline/gutenberg_data.py
@@ -0,0 +1,37 @@
+import os
+import re
+from glob import glob
+from tqdm import tqdm
+import pandas as pd
+
+from llama.constants import DATA_DIR
+
+
+def how_to_get_gutenberg_subset():
+    folder_name = 'gutenberg_dataset_subset'
+    save_path = os.path.join(DATA_DIR, folder_name)
+    print("You can download the dataset from here:\n"
+          "https://shibamoulilahiri.github.io/gutenberg_dataset.html\n"
+          f"Put the unzipped folder in the data directory named '{folder_name}'\n"
+          f"Thus in {save_path}.")
+    return save_path
+
+
+def preprocess_text(text):
+    # text = re.sub(r'(?<!\n)\n(?!\n)', '', text)  # remove single \n
+    text = re.sub(r'\n\n+', '\n\n', text)
+    return text
+
+
+def load_gutenberg_data(data_dir):
+    text_files = glob(os.path.join(data_dir, '**/*.txt'), recursive=True)
+    text_dict = {}
+    for filepath in tqdm(text_files):
+        try:
+            with open(filepath, 'r') as f:
+                text_dict[os.path.basename(filepath)] = preprocess_text(f.read())
+        except UnicodeDecodeError:
+            # for simplicity, we'll ignore files we cannot load
+            pass
+    df_metadata = pd.DataFrame(data=[k.split('___') + [k] for k in text_dict.keys()], columns=['author', 'title', 'id'])
+    return text_dict, df_metadata
