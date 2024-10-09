46a2b0742bd5814bb9df5ca8db910e1dd2e4f7ac
v.glisovic <v.glisovic@fourthline.com>
2024-10-09 21:21:11 +0200

    updated training scheme

--- dreambooth/scripts/autotrain.sh
+++ dreambooth/scripts/autotrain.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+export USE_8BIT_ADAM="False"
+export USE_XFORMERS="False"
+export TRAIN_TEXT_ENCODER="False"
+export DISABLE_GRADIENT_CHECKPOINTING="False"
+
 # make sure the dreambooth python env is activated
 autotrain dreambooth \
   --model "stabilityai/stable-diffusion-xl-base-1.0" \
   --project-name "vuk-SDXL" \
-  --image-path "../data/vuk-photos/crops" \
-  --prompt "A photo of Vuk his smiling face and wearing a cap on his head." \
+  --image-path "../data/vuk-photos/crops/" \
+  --prompt "A photo of Vuk Glisovic wearing casual clothes, taking a selfie, and smiling." \
   --resolution 1024 \
   --batch-size 1 \
   --num-steps 500 \
