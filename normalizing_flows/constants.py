17b9360b19644c5b4aebd79d9b759ffc18d0b448
Vuk Glisovic <v.glisovic@fourthline.com>
2025-03-30 16:16:09 +0200

    created env yaml and constants

---
+++ normalizing_flows/constants.py
@@ -0,0 +1,6 @@
+import os
+
+
+# project information
+PROJECT_NAME = 'normalizing_flows'
+PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
