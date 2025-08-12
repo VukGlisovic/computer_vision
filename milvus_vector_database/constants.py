c90d0bf9c0dc728feddfcf68284aeb9c14666480
Vuk Glisovic <v.glisovic@fourthline.com>
2025-08-12 20:57:32 +0200

    created constants.py

---
+++ milvus_vector_database/constants.py
@@ -0,0 +1,9 @@
+import os
+
+
+# project information
+PROJECT_NAME = 'milvus_vector_database'
+PROJECT_PATH = os.path.join(os.path.realpath(__file__)[:os.path.realpath(__file__).find('/' + PROJECT_NAME)], PROJECT_NAME)
+
+
+DB_NAME = ''
