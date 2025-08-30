c3d4fb17d267486de343a81bddc84e653a908ed4
Vuk Glisovic <v.glisovic@fourthline.com>
2025-08-30 11:43:41 +0200

    plotting ivf sq8 benchmark results

--- milvus_vector_database/scripts/benchmark_ivf_sq8.py
+++ milvus_vector_database/scripts/benchmark_ivf_sq8.py
@@ -20,6 +20,7 @@ There's basically two types of parameters we can play around with:
    accurate the search, but also the slower the search.
 
 """
+from typing import Dict, List
 import os
 import argparse
 import logging
@@ -31,6 +32,55 @@ from milvus_vector_database.constants import PROJECT_PATH
 
 
 logger = logging.getLogger(__name__)
+
+
+def plot_ivf_sq8_results(benchmark_results: Dict[str, List], output_path: str):
+    """Plot the IVF_SQ8 benchmark results.
+
+    The x-axis will be based on the `nlist` parameter from the index config and the
+    different lines will be base don the `nprobe` from the search config.
+    """
+    fig, [ax1, ax2] = plt.subplots(2, 1, figsize=(14, 8))
+    fig.tight_layout(h_pad=5)
+
+    unique_nprobes = np.unique([r.search_config.params['nprobe'] for r in benchmark_results['IVF_SQ8']])
+
+    line_kwargs = {'lw': 2, 'alpha': 0.7}
+    for nprobe in unique_nprobes:
+
+        nprobe_results = [r for r in benchmark_results['IVF_SQ8'] if r.search_config.params['nprobe'] == nprobe]
+        xs = [r.index_config.params['nlist'] for r in nprobe_results]
+        ys_time = [r.get_search_time_avg() * 1000 for r in nprobe_results]
+        ys_acc = [r.get_accuracy() * 100 for r in nprobe_results]
+
+        ax1.plot(xs, ys_time, label=f'nprobe={nprobe}', **line_kwargs)
+        ax2.plot(xs, ys_acc, label=f'nprobe={nprobe}', **line_kwargs)
+
+    ax1.legend()
+    ax1.set_xlabel('nlist')
+    ax1.set_ylabel('search time per query (ms)')
+    ax1.set_ylim(0)
+    ax1.set_title('Benchmark results (search time)')
+    ax1.grid(ls='--', lw=0.5, c='black', alpha=0.4)
+    ax1.set_xscale('log')
+    ax1.set_xticks(xs)
+    ax1.set_xticklabels([str(k) for k in xs])
+    ax1.set_xticks([], minor=True)  # Remove minor ticks
+
+    ax2.legend()
+    ax2.set_xlabel('nlist')
+    ax2.set_ylabel('accuracy (%)')
+    ax2.set_title('Benchmark results (accuracy)')
+    ax2.grid(ls='--', lw=0.5, c='black', alpha=0.4)
+    ax2.set_xscale('log')
+    ax2.set_xticks(xs)
+    ax2.set_xticklabels([str(k) for k in xs])
+    ax2.set_xticks([], minor=True)  # Remove minor ticks
+
+    if _dir := os.path.dirname(output_path):
+        os.makedirs(_dir, exist_ok=True)
+    plt.savefig(output_path)
+    plt.close()
 
 
 def main(output_filename):
@@ -52,9 +102,13 @@ def main(output_filename):
         for nl, np in product(nlist, nprobe)  # First nlist then nprobe because then we don't need to re apply the same index config multiple times
     ]
 
+    # Setup and run benchmark
     benchmark = MilvusGloveBenchmark()
     benchmark.benchmark_configs(configs, k=10)
+
+    # Save benchmark results
     benchmark.save_benchmark_results(os.path.join(PROJECT_PATH, 'data', f'{output_filename}.pkl'))
+    plot_ivf_sq8_results(benchmark.benchmark_results, os.path.join(PROJECT_PATH, 'data', f'{output_filename}.jpeg'))
 
 
 if __name__ == "__main__":
