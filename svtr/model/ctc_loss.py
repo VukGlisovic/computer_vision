2d6ed61c23ae1fb77a718fda06a7fd0e1affb38a
Vuk Glisovic <v.glisovic@fourthline.com>
2026-03-29 18:49:06 +0200

    added typing in ctc loss

--- svtr/model/ctc_loss.py
+++ svtr/model/ctc_loss.py
@@ -3,35 +3,26 @@ import torch
 
 class CTCLoss:
 
-    def __init__(self, blank=0, reduction='mean', zero_infinity=False):
+    def __init__(self, blank: int = 0, reduction: str = 'mean', zero_infinity: bool = False):
         """https://pytorch.org/docs/stable/generated/torch.nn.CTCLoss.html
-
-        Args:
-            blank (int):
-            reduction (str):
-            zero_infinity (bool):
         """
         self.ctc_loss = torch.nn.CTCLoss(blank=blank, reduction=reduction, zero_infinity=zero_infinity)
         self.avg_loss = -1
         self.n = 0
 
-    def __call__(self, y_pred, y_true, input_lengths, target_lengths, *args, **kwargs):
-        """
-        Args:
-            y_pred (tensor):
-            y_true (tensor):
-            input_lengths (tensor):
-            target_lengths (tensor):
-        
-        Returns:
-            tensor: a scalar representing the loss value.
-        """
+    def __call__(self,
+                 y_pred: torch.Tensor,
+                 y_true: torch.Tensor,
+                 input_lengths: torch.Tensor,
+                 target_lengths: torch.Tensor,
+                 *args,
+                 **kwargs) -> torch.Tensor:
         y_pred = y_pred.permute((1, 0, 2))  # CTC loss requires first dim to be timesteps dim
         loss = self.ctc_loss(y_pred, y_true, input_lengths, target_lengths)
         self.update(loss.item(), y_true.shape[0])
         return loss
 
-    def update(self, loss, bs):
+    def update(self, loss: float, bs: int):
         """Incrementally update the average loss.
         """
         new_n = self.n + bs
